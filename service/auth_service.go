e1e44eb580eb8b398e8714733cd48781f2fad941
Yu <yu@matrix.works>
2025-09-18 10:24:24 +0800

    remove some log

--- service/auth_service.go
+++ service/auth_service.go
@@ -68,19 +68,19 @@ func (s *AuthService) HandleCallback(provider, userID, code string) error {
 
 // 获取 access_token
 func (s *AuthService) GetAccessToken(provider, userID string) (*oauth2.Token, error) {
-	fmt.Printf("provider: %s, userID: %s\n", provider, userID)
+	//fmt.Printf("provider: %s, userID: %s\n", provider, userID)
 	client, err := model.GetOAuthClient(s.DB, provider)
 	if err != nil {
 		return nil, err
 	}
 	conf := client.ToConfig()
-	fmt.Printf("client id: %s\n", conf.ClientID)
+	//fmt.Printf("client id: %s\n", conf.ClientID)
 
 	t, err := model.GetToken(s.DB, provider, userID)
 	if err != nil {
 		return nil, err
 	}
-	fmt.Printf("RefreshToken: %s\n", t.RefreshToken)
+	//fmt.Printf("RefreshToken: %s\n", t.RefreshToken)
 	refreshTokenBytes, err := util.Decrypt(t.RefreshToken)
 	if err != nil {
 		return nil, err
