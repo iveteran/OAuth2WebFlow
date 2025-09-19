739a03506da0102a79a82d64438cc0d818a038df
Yu <yu@matrix.works>
2025-09-19 18:29:19 +0800

    feat: support to show access log

--- main.go
+++ main.go
@@ -5,12 +5,49 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/iveteran/OAuth2WebFlow/controller"
 	"github.com/iveteran/OAuth2WebFlow/service"
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+// 自定义ResponseWriter用于捕获状态码
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
+
+// 日志中间件
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		// 创建一个自定义的ResponseWriter来捕获状态码
+		lrw := &loggingResponseWriter{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+
+		// 执行下一个处理器
+		next.ServeHTTP(lrw, r)
+
+		// 记录日志
+		log.Printf("%s %s %d %v %s",
+			r.Method,
+			r.RequestURI,
+			lrw.statusCode,
+			time.Since(start),
+			r.RemoteAddr,
+		)
+	})
+}
 
 func main() {
 	db, err := sql.Open("sqlite3", "./oauth2.db")
@@ -23,10 +60,14 @@ func main() {
 
 	authService.InitDB()
 
-	http.HandleFunc("/authorize", authController.Authorize)
-	http.HandleFunc("/callback", authController.Callback)
-	http.HandleFunc("/get_token", authController.GetToken)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/authorize", authController.Authorize)
+	mux.HandleFunc("/callback", authController.Callback)
+	mux.HandleFunc("/get_token", authController.GetToken)
+
+	// 使用日志中间件包装处理器
+	loggedMux := loggingMiddleware(mux)
 
 	fmt.Println("Server running at http://localhost:9090")
-	log.Fatal(http.ListenAndServe(":9090", nil))
+	log.Fatal(http.ListenAndServe(":9090", loggedMux))
 }
